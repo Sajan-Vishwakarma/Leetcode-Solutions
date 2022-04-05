2ebc17d3b12d86f14448c2619c0d4be39fbf3d42
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-04-05 15:37:03 +0530

    Time: 559 ms (71.59%), Space: 107.6 MB (84.70%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,66 +1,66 @@
 class Solution {
 public:
     
-    
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
         
-        
-        auto isreachable=[&](vector<vector<bool>> &mat)->bool{
-            queue<pair<int,int>> q;
-            for(int j=0;j<col;j++){
-                if( !mat[0][j] ) q.push({0,j});
-            }  
-            
-            int dx[]={-1,1,0,0};
-            int dy[]={0,0,1,-1};
-            
-            auto valid=[&](int x,int y){
-                if(x < 0 || y < 0 || x >= row || y >= col)
-                    return false;
-                if( mat[x][y]) return false;
-                return true;
-            };
-            
-            while(!q.empty()){
-                auto [x,y] = q.front();
-                q.pop();
-                
-                mat[x][y] = true;
-                if(x == row-1) return true;
-                
-                for(int i=0;i<4;i++){
-                    int newx = x+dx[i], newy = y+dy[i];
-                    if(valid(newx,newy)){
-                        mat[newx][newy] = true;
-                        q.push({newx,newy});
-                    }
-                }
-            }
-            
-            return false;
-        };
-        
-        auto CHECK=[&](int index){
-            vector<vector<bool>> matrix(row,vector<bool>(col,false));
-            for(int i=0;i<=index;i++){
-                matrix[cells[i][0]-1][cells[i][1]-1] = true;
-            }
-            return isreachable(matrix);
-        };
-        
-        int low = 0, high = cells.size()-1, ans = 0;
-        while( low <= high){
-            int mid = (low+high)/2;
-            cout<<mid<<" ";
-            if(CHECK(mid)){
-                ans = mid+1;
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
+        int N = row*col+2;
+        vector<int> dsu_par(N), dsu_size(N,1);
+        for(int i=1;i<N;i++){
+            dsu_par[i] = i;
         }
         
-        return ans;
+        function<int(int)> find=[&](int u){
+            if( dsu_par[u] == u)
+                return u;
+            return dsu_par[u] = find(dsu_par[u]);
+        };
+        
+        auto connected=[&](int u,int v){
+            return find(u) == find(v);
+        };
+        
+        auto merge=[&](int u,int v){
+            
+            int root1 = find(u), root2 = find(v);
+            if( root1 == root2 ) return;
+            
+            if(dsu_size[root1] < dsu_size[root2]){
+                dsu_size[root2] += dsu_size[root1];
+                dsu_par[root1] = root2;
+            }
+            else{
+                dsu_size[root1] += dsu_size[root2];
+                dsu_par[root2] = root1;                
+            }  
+        };
+        
+        // dsu part over -----****-------
+        
+        int dummy1 = N-2, dummy2 = N-1;
+        for(int j=0;j<col;j++){
+            merge(dummy1, j);
+            merge(dummy2, (row-1)*col+j);
+        }
+        
+        vector<vector<bool>> matrix(row,vector<bool>(col,false));
+        vector<pair<int,int>> directions={{0,1},{0,-1},{-1,0},{1,0}};
+        
+        for(int i=cells.size()-1;i>=0;i--){
+            int r = cells[i][0]-1, c = cells[i][1]-1;
+            matrix[r][c] = true;
+            
+            int node = r*col + c;
+            for(auto [dx,dy]:directions){
+                int newx = r+dx, newy =c+dy;
+                if( newx<0 || newx>=row || newy<0 || newy>=col)
+                    continue;
+                if( matrix[newx][newy] == true){
+                    merge(r*col+c, newx*col+newy);
+                }
+                if( connected(dummy1,dummy2))
+                    return i;
+            }
+        }
+        return 0;    
     }
 };
