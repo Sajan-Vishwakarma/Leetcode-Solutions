ca562c3a12d1059b3d810eb563da3d0813e28fd1
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-04-28 22:43:02 +0530

    Added solution - LeetHub

--- Rearrange an array with O(1) extra space - GFG/rearrange-an-array-with-o1-extra-space.cpp
+++ Rearrange an array with O(1) extra space - GFG/rearrange-an-array-with-o1-extra-space.cpp
@@ -15,12 +15,13 @@ class Solution{
     void arrange(long long arr[], int n) {
         // Your code here
         // First step: Increase all values by (arr[arr[i]]%n)*n
-    for (int i=0; i < n; i++)
-        arr[i] += (arr[arr[i]]%n)*n;
+        long long val = 1e6;
+        for (int i=0; i < n; i++)
+            arr[i] += (val*(arr[arr[i]]%val));
 
-    // Second Step: Divide all values by n
-    for (int i=0; i<n; i++)
-        arr[i] /= n;
+        // Second Step: Divide all values by n
+        for (int i=0; i<n; i++)
+            arr[i] /= val;
     }
 };
 
