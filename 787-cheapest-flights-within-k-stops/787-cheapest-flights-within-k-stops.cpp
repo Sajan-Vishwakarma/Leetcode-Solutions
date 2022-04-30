9075275e8b06ea87f3bec7122baa26b93ea9dafa
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-04-30 17:56:16 +0530

    Time: 297 ms (8.51%), Space: 49.6 MB (8.79%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int source, int destination, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& edges, int source, int destination, int k) {
         
-        int m = flights.size();
-        vector<vector<pair<int,int>>> graph(n);
-        for(int i=0;i<m;i++){
-            int u, v , wt;
-            u = flights[i][0], v = flights[i][1], wt = flights[i][2];
-            graph[u].push_back({v,wt});
-        }
-                
+        int m = edges.size();
+        
         const int INF = 1e7;
-	
-        int dp[n][k+2];
-        memset(dp,-1,sizeof(dp));
-    
-        function<int(int,int)> dfs= [&](int node,int k)->int{
-    
-            if(node == destination) return 0;
-            if(k == 0) return INF;
-            
-            if(dp[node][k] != -1) return dp[node][k];
+        vector<int> dist(n,INF);
+        dist[source] = 0;
         
-            int mincost = INF;
-            for(auto &[neighbor,p]:graph[node]){
-                mincost = min(mincost, p + dfs(neighbor,k-1));
+        for(int i=0;i<=k;i++){
+            vector<int> newdist = dist;
+            for( auto vec: edges){
+                int u = vec[0], v= vec[1], p = vec[2];
+                if( dist[u] == INF) continue;
+                if( newdist[v] > dist[u] + p)
+                    newdist[v] = dist[u]+p;
             }
-            return dp[node][k] = mincost;
-        };
+            dist = newdist;
+        }
     
-        int ans = dfs(source,k+1);
-        if(ans == INF) return -1;
-        
+        int ans = dist[destination];
+        if(dist[destination] == INF)
+            ans = -1;
+	// cout<<ans<<endl;
         return ans;
     }
 };
