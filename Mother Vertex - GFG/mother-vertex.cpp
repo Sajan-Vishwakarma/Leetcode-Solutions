300de55f6c2c41abace3365700fb8163a99e268e
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-05-05 19:45:19 +0530

    Added solution - LeetHub

---
+++ Mother Vertex - GFG/mother-vertex.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution 
+{
+    public:
+    //Function to find a Mother Vertex in the Graph.
+	int findMotherVertex(int n, vector<int> graph[]){
+	    // Code here
+    	function<int(int,vector<bool>&)> dfs= [&](int node,vector<bool> &visited)->int{
+    		int count = 1;
+    		visited[node] = true;
+    		for(int &child:graph[node]){
+    			if(!visited[child])
+    				count += dfs(child,visited);
+    		}
+    		return count;
+    	};
+    
+    	int answernode = 0;
+    	vector<bool> visited(n,false);
+    	for(int node=0;node<n;node++){
+    		if(!visited[node]){
+    			dfs(node,visited);
+    			answernode = node;
+    		}
+    	}
+    
+    	visited.assign(n,false);
+    	int visNodeCount = dfs(answernode,visited);
+    
+    	if( visNodeCount != n){
+    		answernode = -1;
+    	}
+    // 	cout<<answernode<<endl;
+        return answernode;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+		}
+		Solution obj;
+		int ans = obj.findMotherVertex(V, adj);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
