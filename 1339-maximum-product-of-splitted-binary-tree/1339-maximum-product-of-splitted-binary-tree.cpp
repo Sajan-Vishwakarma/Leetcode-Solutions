544352fb7e94131b95340b62c3dd30db22c008e4
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-05-08 17:47:34 +0530

    Time: 136 ms (79.58%), Space: 77.2 MB (92.15%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,28 +1,37 @@
-class Solution {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution{
 public:
-    long long bfs(TreeNode* root){
-        queue<TreeNode*> q;
-        q.push(root);
-        long long total = root->val, ans = 0;
-        while(!q.empty()){
-            TreeNode* curr = q.front();
-            q.pop();
-            long long subtree = curr->val;
-            ans = max(ans, (total-subtree)*subtree);
-            if(curr->left) q.push(curr->left);
-            if(curr->right) q.push(curr->right);
-        }
-        return ans;
-    }
-    
-    int getsum(TreeNode* root){
+    const long long MOD = 1e9+7;
+    int getSubtreeSum(TreeNode* root){
         if(root == NULL) return 0;
-        return root->val += getsum(root->left) + getsum(root->right);
+        return root->val += getSubtreeSum(root->left) + getSubtreeSum(root->right);
+    }
+
+    void dfs(TreeNode*root,long long totalsum,long long &ans){
+        if(!root) return;
+
+        long long subtree = root->val;
+        ans = max(ans, (totalsum-subtree)*subtree);
+        
+        dfs(root->left,totalsum,ans);
+        dfs(root->right,totalsum,ans);
     }
 
     int maxProduct(TreeNode* root) {
-        getsum(root);
-        long long ans = bfs(root);
-        return ans % int(pow(10,9)+7);
+        long long totalsum = getSubtreeSum(root);
+
+        long long ans=0;
+        dfs(root,totalsum,ans);
+        return ans%MOD;
     }
 };
