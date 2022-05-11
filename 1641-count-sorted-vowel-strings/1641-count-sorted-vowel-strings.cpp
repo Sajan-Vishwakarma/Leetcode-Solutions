35aa0c4bf33e9fc59197f3040f33e0badd7d6434
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-05-11 09:26:25 +0530

    Time: 7 ms (19.04%), Space: 6.1 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -4,29 +4,18 @@ public:
 
     int countVowelStrings(int n) {
         
-        map<char,vector<char>> mp;  
-        mp['a'] = {'a','e','i','o','u'};
-        mp['e'] = {'e','i','o','u'};
-        mp['i'] = {'i','o','u'};
-        mp['o'] = {'o','u'};
-        mp['u'] = {'u'};
-
-        
-        function<int(char,int)> fun=[&](char ch,int len)->int{    
-            if(len == n) return 1;
-        
-            int ans = 0;
-            for(char nextch:mp[ch]){
-                ans += fun(nextch,len+1);
-            }
-            return ans;
-        };
-        
-        int ans = 0;
-        for(char ch='a';ch<='z';ch++){
-            if(mp.find(ch) == mp.end()) continue;
-            ans += fun(ch,1);
+        vector<int> dp{1,1,1,1,1};
+ 
+        for(int i=1;i<n;i++){
+            vector<int> newdp(5,0);
+            newdp[0] += dp[0]+dp[1]+dp[2]+dp[3]+dp[4];
+            newdp[1] += dp[1]+dp[2]+dp[3]+dp[4];
+            newdp[2] += dp[2]+dp[3]+dp[4];
+            newdp[3] += dp[3]+dp[4];
+            newdp[4] += dp[4];
+            swap(dp,newdp);
         }
-        return ans;
+        
+        return accumulate(dp.begin(),dp.end(),0);
     }
 };
