4edee249aee1ecf36fda22db488fdc4b1231a8b2
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-05-11 09:28:16 +0530

    Time: 3 ms (45.39%), Space: 6 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -8,11 +8,10 @@ public:
  
         for(int i=1;i<n;i++){
             vector<int> newdp(5,0);
-            newdp[0] += dp[0]+dp[1]+dp[2]+dp[3]+dp[4];
-            newdp[1] += dp[1]+dp[2]+dp[3]+dp[4];
-            newdp[2] += dp[2]+dp[3]+dp[4];
-            newdp[3] += dp[3]+dp[4];
-            newdp[4] += dp[4];
+            for(int ch=0;ch<5;ch++){
+                for(int newch=ch;newch<5;newch++)
+                    newdp[ch] += dp[newch];    
+            }
             swap(dp,newdp);
         }
         
