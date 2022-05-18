fea14550f93aeb29e45254cad1c4b01d6b3f74e7
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-05-18 14:14:44 +0530

    Time: 149 ms (65.76%), Space: 49.2 MB (90.95%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,17 +1,10 @@
+using vvs =  vector<vector<string>>;
+using vvb = vector<vector<bool>>;
 class Solution {
 public:
     
-    bool palindrome(string s){
-        int l=0,r=s.size()-1;
-        while(l <= r){
-            if(s[l] != s[r]) return false;
-            l++;
-            r--;
-        }
-        return true;
-    }
     
-    void fun(int pos,string &s,vector<string> &temp,vector<vector<string>> &ans){
+    void fun(int pos,string &s,vector<string> &temp,vvs &ans,vvb &isPal){
         if(pos == s.size()){
             ans.push_back(temp);
             return;
@@ -21,19 +14,38 @@ public:
         for(int i=pos;i<s.size();i++){
             str  += s[i];
             
-            if( palindrome(str) ){
+            if( isPal[pos][i] ){
                 temp.push_back(str);
-                fun(i+1,s,temp,ans);
+                fun(i+1,s,temp,ans,isPal);
                 temp.pop_back();
             }
         }         
-        
     }
     
     vector<vector<string>> partition(string s) {
+        
+        int n = s.size();
+        vector<vector<bool>> isPal(n+1,vector<bool>(n+1,false));
+        for(int i=0 ;i<n;i++){
+            isPal[i][i] = true;
+            if(i>0) isPal[i-1][i] = (s[i-1]==s[i]);
+        }
+        
+        for(int len=3;len<=s.size();len++){
+            for(int i=0;i<n;i++){
+                int j = i+len-1;
+                if(j >= n) break;
+                isPal[i][j] = isPal[i+1][j-1] && (s[i] == s[j]);
+            }
+        }
+//         aab
+//         TT*
+//         *T*
+//         **T
+        
         vector<vector<string>> ans;
         vector<string> temp;
-        fun(0,s,temp,ans);
+        fun(0,s,temp,ans,isPal);
         return ans;
     }
 };
