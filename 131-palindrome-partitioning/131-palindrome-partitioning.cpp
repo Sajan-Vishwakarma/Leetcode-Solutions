e2972ca8e52721d9660d386c25483dbaa9c1d028
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-05-18 14:04:52 +0530

    Time: 116 ms (86.23%), Space: 49.2 MB (90.95%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    void fun(int l,int n,vector<string> &v,string &s){
-        if(l == n){
-            ans.push_back(v);
+    
+    bool palindrome(string s){
+        int l=0,r=s.size()-1;
+        while(l <= r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
+    void fun(int pos,string &s,vector<string> &temp,vector<vector<string>> &ans){
+        if(pos == s.size()){
+            ans.push_back(temp);
             return;
         }
         
-        auto valid = [&](string &s)->bool{
-            int l = 0, r = s.size()-1;
-            while(l <= r){
-                if(s[l] != s[r]) return false;
-                l++;
-                r--;
-            }
-            return true;
-        };
-        
         string str="";
-        for(int i=l;i<n;i++){
-            str += s[i];
-            if(valid(str)){
-                v.push_back(str);
-                fun(i+1,n,v,s);
-                v.pop_back();
+        for(int i=pos;i<s.size();i++){
+            str  += s[i];
+            
+            if( palindrome(str) ){
+                temp.push_back(str);
+                fun(i+1,s,temp,ans);
+                temp.pop_back();
             }
-        }
+        }         
+        
     }
+    
     vector<vector<string>> partition(string s) {
-        vector<string> v;
-        fun(0,s.size(),v,s);
+        vector<vector<string>> ans;
+        vector<string> temp;
+        fun(0,s,temp,ans);
         return ans;
     }
 };
