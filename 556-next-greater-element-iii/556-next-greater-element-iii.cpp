ee50456b6e9252dcd9360f68fa45643e34dd4587
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-06-01 18:56:18 +0530

    Time: 5 ms (8.30%), Space: 5.9 MB (93.50%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -4,23 +4,8 @@ public:
 
     int nextGreaterElement(int num) {
 
-        string s = to_string(num), intmax="2147483647";
+        string s = to_string(num);
         int n = s.size();
-
-        auto compare=[&](string s,string intmax){
-            if( s.size() > intmax.size()) return true;
-            if( intmax.size() > s.size()) return false;
-
-            for(int i=0;i<n;i++){
-                if( s[i] > intmax[i]){
-                    return true;
-                }
-                else if(s[i] < intmax[i]) {
-                    return false;
-                }
-            }
-            return false;
-        };
 
         int i,j,k;
         for( i=n-1; i > 0;i--){
@@ -28,17 +13,19 @@ public:
                 break;
             }
         }
-        // number is in decreasing so next number doesn't exists
+        // number is in decreasing so next number exists
         if( i == 0) return -1;
 
         k = i;
         for( j=i ; j < n; j++){
-            if( s[j] > s[i-1] && s[j] <= s[k]){
+            if( s[j] > s[i-1] && s[j] < s[k]){
                 k = j;
             }
         }        
         swap(s[i-1],s[k]);
         sort(s.begin()+i,s.end());
-        return compare(s,intmax)?-1:stoi(s);
+
+        long long ans = stoll(s);
+        return ( ans > (1LL<<31)-1)?-1: int(ans);
     }
 };
