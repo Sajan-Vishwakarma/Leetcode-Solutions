259cfd74c1181fffdac5a10b7b73b750b1df67a9
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-06-03 10:47:28 +0530

    Added solution - LeetHub

---
+++ Array Partition - GFG/array-partition.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool partitionArray(int n, int k, int m, vector<int> &a){
+        // code here
+        sort(a.begin(),a.end());
+        
+        int dp[n];
+        memset(dp,-1,sizeof(dp));
+        
+        function<int(int)> fun=[&](int curr){
+            
+            if( curr >= n) 
+                return 1;
+                
+            if( dp[curr] != -1)
+                return dp[curr];
+            
+            bool ok = false;
+            for(int i=curr+k-1;i<n;i++){
+                if(abs(a[i]-a[curr]) > m) break;
+                ok |= fun(i+1);
+                if(ok) return 1;
+            }
+            return dp[curr] = ok;
+        };
+        
+        return fun(0);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N, K, M;
+        cin >> N >> K >> M;
+        vector<int> A(N);
+        for(int i = 0; i < N; i++){
+            cin >> A[i];
+        }
+        Solution obj;
+        bool ans = obj.partitionArray(N, K, M, A);
+        if(ans){
+            cout << "YES\n";
+        }
+        else{
+            cout<< "NO\n";
+        }
+    }
+}
+  // } Driver Code Ends
