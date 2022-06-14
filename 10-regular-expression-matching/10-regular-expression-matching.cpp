5a9c3b168ae1c59b176457673a72957697704ae6
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-06-14 14:52:29 +0530

    Time: 167 ms (16.45%), Space: 7 MB (49.09%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,42 +1,60 @@
+using vvb = vector<vector<bool>>;
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        
         int n = s.size(), m = p.size();
-        
-        vector<vector<int>> dp(n+10,vector<int>(m+10,-1));
-        
-        function<bool(int,int,vector<vector<int>>&)> suffixMatch=[&](int i,int j,vector<vector<int>> &dp)->bool{           
-            if( i == n && j == m) return true;
-            if( j == m ) return false;
-            if( i == n ){
+
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,NULL));
+
+        function<bool(int,int,vvb&)> fun= [&](int i,int j,vvb &dp)->bool{
+
+            if(i == n && j == m) return true;
+            if(i>n || j >= m) return false;
+
+            if(i == n){
+                // checking remaining pattern
                 for(int k=j; k < m; k+=2){
                     if( k+1 >= m || p[k+1] != '*'){
                         return false;
                     }
                 }
                 return true;
-            }        
-            
-            int &ans = dp[i][j];
-            if(ans != -1) return ans;
-            
-            if( j+1 < m && p[j+1] == '*'){ 
-                int k = 0;
-                while(true){
-                    if( suffixMatch(i+k,j+2,dp) ) return ans = true;
-                    if(i+k >= n) break;
-                    if( p[j] != '.' && s[i+k] != p[j] ) break;
-                    k++;
+            }
+
+            if(dp[i][j] != NULL)
+                return dp[i][j];
+
+            bool ans = false;
+
+            if(j+1<m && p[j+1] == '*'){
+                
+                // matching 0 characters
+                ans |= fun(i,j+2,dp); 
+                
+                // 1 or more characters of same p[j] match
+                for(int match=0; (i+match < n) && (p[j]==s[i+match]); match++){ 
+                    ans |= fun(i+match+1,j+2,dp);  
+                    if(ans) return dp[i][j] = ans;
                 }
-                return ans = false;
+
+                // matching more than 1 characters i.e '.*'
+                if(p[j] == '.'){
+                    for(int match=1;match<=n;match++){
+                        ans |= fun(i+match,j+2,dp);
+                        if(ans) return dp[i][j] = ans;
+                    } 
+                }
             }
             
-            if( s[i] == p[j] || p[j] == '.')
-                return ans = suffixMatch(i+1,j+1,dp);
+            if(s[i] == p[j] || p[j] == '.' ){
+                ans |= fun(i+1,j+1,dp);
+            }
             
-            return ans = false;    
+            return dp[i][j] = ans;
         };
-        
-        return suffixMatch(0,0,dp);
+
+        return fun(0,0,dp);
     }
 };
