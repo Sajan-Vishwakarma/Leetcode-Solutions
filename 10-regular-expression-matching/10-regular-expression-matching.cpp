f29947c9ee8eb38258d02552f13c2688c5ca5fdf
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-06-14 14:50:36 +0530

    Time: 285 ms (6.18%), Space: 6.9 MB (61.24%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,3 +1,5 @@
+using vvb = vector<vector<bool>>;
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
@@ -6,7 +8,7 @@ public:
 
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,NULL));
 
-        function<bool(int,int)> fun= [&](int i,int j)->bool{
+        function<bool(int,int,vvb&)> fun= [&](int i,int j,vvb &dp)->bool{
 
             if(i == n && j == m) return true;
             if(i>n || j >= m) return false;
@@ -26,23 +28,23 @@ public:
 
             bool ans = false;
             if(s[i] == p[j] || p[j] == '.' ){
-                ans |= fun(i+1,j+1);
+                ans |= fun(i+1,j+1,dp);
             }
 
             if(j+1<m && p[j+1] == '*'){
                 
-                ans |= fun(i,j+2); // matching 0 characters
+                ans |= fun(i,j+2,dp); // matching 0 characters
                 
                 // 1 or more characters of same p[j] match
                 for(int match=0; (i+match < n) && (p[j]==s[i+match]); match++){ 
-                    ans |= fun(i+match+1,j+2);  
+                    ans |= fun(i+match+1,j+2,dp);  
                     if(ans) return dp[i][j] = ans;
                 }
 
                 // matching more than 1 characters i.e '.*'
                 if(p[j] == '.'){
                     for(int match=1;match<=n;match++){
-                        ans |= fun(i+match,j+2);
+                        ans |= fun(i+match,j+2,dp);
                         if(ans) return dp[i][j] = ans;
                     } 
                 }
@@ -50,6 +52,6 @@ public:
             return dp[i][j] = ans;
         };
 
-        return fun(0,0);
+        return fun(0,0,dp);
     }
 };
