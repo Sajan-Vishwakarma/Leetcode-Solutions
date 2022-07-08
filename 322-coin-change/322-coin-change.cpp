b740ab574cb1984d318c18a883a82a222bc3de67
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-07-08 16:01:29 +0530

    Time: 142 ms (46.16%), Space: 19.4 MB (27.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,9 +4,6 @@ public:
         
         sort(coins.begin(),coins.end());
         vector<int> dp(10005,1e5);
-        for(int i:coins) 
-            if(i<=amount) dp[i] = 1;
-        
         dp[0] = 0;
         for(long long t=0;t<=amount;t++){
             for(int c:coins){
