f85b8a81756306066395106049c1f40f2ba33324
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-08-01 13:08:56 +0530

    Time: 107 ms (6.60%), Space: 10.3 MB (11.74%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
-
-        vector<vector<int>> maximum(n,vector<int>(n));
-        vector<vector<int>> product(n,vector<int>(n,1e5));
-
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            maximum[i][i] = product[i][i] = arr[i];
-            sum += arr[i];
+    map<pair<int,int>, pair<int,int>> dp;
+    pair<int,int> solve(int st, int en, vector<int> &a) {
+        if(st == en) {
+            return {a[st], 0}; // maxleaf, minSum
         }
-
-        for(int len=2;len<=n;len++){
-            for(int i=0;i<n;i++){
-                int j = i+len-1;
-                if(j >= n) break;
-
-                for(int k=i;k<j;k++){
-                    maximum[i][j] = max({maximum[i][j],maximum[i][k],maximum[k+1][j]});
-                }
-                for(int k=i;k<j;k++){
-                    product[i][j] = min(product[i][j],maximum[i][k]*maximum[k+1][j]+product[i][k]+product[k+1][j]);
-                }
-            }
+        if(dp.count({st,en})) return dp[{st,en}];
+        int maxLeaf = INT_MIN, minSum = INT_MAX;
+        for(int j = st; j < en; j++) {
+            auto left = solve(st,j,a);
+            auto right = solve(j+1,en,a);
+            maxLeaf = max(left.first, right.first);
+            minSum = min(minSum, left.first * right.first + left.second + right.second);
         }
-        return product[0][n-1] - sum;
+        return dp[{st,en}] = {maxLeaf, minSum};
     }
-
+    
+    int mctFromLeafValues(vector<int>& a) {
+        // memset(dp,-1,sizeof(dp));
+        int n = a.size();
+        return solve(0,n-1,a).second;
+    }
 };
