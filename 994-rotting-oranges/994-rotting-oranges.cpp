24eaf19a5e7d7a2d0bfbac833b57a872f1ab6ea3
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-08-12 18:07:47 +0530

    Time: 15 ms (37.73%), Space: 12.8 MB (98.18%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        
-        int n= grid.size(), m = grid[0].size();
-        queue<array<int,3>> q;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j,0});
-                }
-            }
-        }
-        
-        vector<pair<int,int>> directions={{-1,0},{1,0},{0,1},{0,-1}};
-        
-        auto valid=[&](int x,int y){
-            if(x<0||y<0||x>=n||y>=m)
-                return false;
-            return grid[x][y] == 1;
-        };
-        
-        int ans = 0;
-        
-        while(!q.empty()){
-            
-            auto [x,y,time] = q.front();
-            q.pop();
-            
-            grid[x][y] = 0;
-            ans = max(ans, time);
-            
-            for(auto &[dx,dy]:directions){
-                int newx = x+dx, newy=y+dy;
-                if( valid(newx,newy) ){
-                    grid[newx][newy] = 0;
-                    q.push({newx,newy,time+1});
-                }
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-        
-        return ans;
-    }
+    int rot(vector<vector<int>>& g, int i, int j, int d) {
+  if (i < 0 || j < 0 || i >= g.size() || j >= g[i].size() || g[i][j] != 1) return 0;
+  g[i][j] = d + 3;
+  return 1;
+}
+int orangesRotting(vector<vector<int>>& g, int d = 0, int fresh = 0) {
+  for (auto i = 0; i < g.size(); ++i) 
+    fresh += count_if(begin(g[i]), end(g[i]), [](int j) { return j == 1; });
+  for (auto old_fresh = fresh; fresh > 0; ++d) {
+    for (auto i = 0; i < g.size(); ++i)
+      for (auto j = 0; j < g[i].size(); ++j)
+        if (g[i][j] == d + 2)
+          fresh -= rot(g,i+1,j,d) + rot(g,i-1,j,d) + rot(g,i,j+1,d) + rot(g,i,j-1,d);
+    if (fresh == exchange(old_fresh, fresh)) return -1;
+  }
+  return d;
+}
 };
