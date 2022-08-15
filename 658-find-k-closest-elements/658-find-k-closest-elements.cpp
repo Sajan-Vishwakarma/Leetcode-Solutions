64f76d210aa3edb80cc16e89aed8284f5fd70809
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-08-15 23:16:45 +0530

    Time: 85 ms (59.95%), Space: 31.7 MB (75.80%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -5,7 +5,6 @@ public:
         const int inf = 1e9;
         int idx = (lower_bound(arr.begin(),arr.end(),x)) - arr.begin();
         
-        vector<int> res;
         int i = idx-1, j = idx;
         while(k--){
             int diff1 = inf, diff2 = inf;
@@ -13,15 +12,15 @@ public:
             if( j < arr.size()) diff2 = abs(arr[j] - x);
             
             if( diff1 <= diff2){
-                res.push_back(arr[i]);
                 i--;
             }
             else{
-                res.push_back(arr[j]);
                 j++;
             }
         }
-        sort(res.begin(),res.end());
+        
+        vector<int> res;
+        for(int it=i+1;it<j;it++) res.push_back(arr[it]);
         return res;
     }
 };
