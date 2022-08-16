4ae0b435d7f4a2732a9d21652e20e96ae49329cb
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-08-16 17:11:28 +0530

    Time: 2 ms (58.15%), Space: 8.1 MB (50.61%) - LeetHub

--- 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
+++ 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
@@ -3,37 +3,36 @@ public:
     int atMostNGivenDigitSet(vector<string>& digits, int num) {
         
         int mask = 0;
-        for(auto &d:digits){
+        for(auto d:digits){
             mask |= (1<<stoi(d));
         }   
         
         string s = to_string(num);
         int n = s.size();
 
-        int dp[11][2];
-        memset(dp,-1,sizeof(dp));
+        vector<vector<int>> dp(10,vector<int>(2,-1));
 
         int start = 0;
-        function<int(short,bool)> fun= [&](short pos,bool tight)->int{
+        function<int(int,int)> fun= [&](int pos,int tight)->int{
             if( pos >= n) return 1;
 
-            if( dp[pos][tight] != -1) return dp[pos][tight];
+            int &ans = dp[pos][tight];
+            if(ans != -1) return ans;
 
             int maxD = (tight)?s[pos]-'0':9;
             int begD = (pos==start)?1:0;
             
-            int ans = 0;
+            ans = 0;
             for(int d=begD;d<=maxD;d++){
                 if( (mask>>d)&1 ) 
                     ans += fun(pos+1,(tight && (d==s[pos]-'0')));
             }
-            return dp[pos][tight] = ans;
+            return ans;
         };
 
         int ans = 0;
         ans += fun(0,true);
         for(int i=1;i<n;i++){
-            start = i;
             ans += fun(i,false);
         }
         
