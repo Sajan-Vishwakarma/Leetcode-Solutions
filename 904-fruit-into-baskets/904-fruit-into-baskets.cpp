babe6cb22771e4e9ad51f5f3c42151bc9498af7a
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-08-23 02:52:31 +0530

    Time: 241 ms (56.67%), Space: 75.2 MB (18.19%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -2,43 +2,20 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         
-        set<int> st;
-        map<int,int> mp;
-        int n= fruits.size();
-        
         int ans = 0;
-        for(int i=0;i<n;i++){
-            int j = i;
-            while( j < n ){
-                st.insert(fruits[j]);
-                mp[fruits[j]] = j;
-                
-                if(st.size() == 3)
-                    break;
-                j++;
-            }
-            ans = max(ans,j-i);
-            if( st.size() <= 2){
-                break;
-            }
-            
-            int next = mp[fruits[i]], next2 = 0, second = 0;            
-            for(auto x:st){
-                if( x == fruits[i] || x == fruits[j]) continue;
-                next2 = mp[x];
-                second = x;
-            }
-            
-            if(next2 < next){
-                st.erase(second);
-                i = next2;
-            }
-            else{
-                st.erase(fruits[i]);
-                i = next;
-            }
-        }
+        map<int,int> st;
+        int i=0, j =0 , n = fruits.size();
         
+        for( ; j < n; j++){
+            st[fruits[j]]++;
+            while( i <= j && st.size() > 2){
+                st[fruits[i]]--;
+                if( st[fruits[i]] == 0)
+                    st.erase(fruits[i]);
+                i++;
+            }
+            ans = max(ans, j-i+1);
+        }
         return ans;
     }
 };
