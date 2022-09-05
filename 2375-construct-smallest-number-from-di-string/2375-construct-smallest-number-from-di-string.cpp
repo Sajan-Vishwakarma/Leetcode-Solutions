60ecfc40d4b5b7cba982e209625ed150b6cad4e8
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2022-09-05 11:19:20 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.35%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        string res; 
+        stack<int> s;
+
+        for (int i = 0; i <= pattern.length(); i++){
+            s.push(i + 1);
+
+            if (i == pattern.length() || pattern[i] == 'I'){
+                while (!s.empty()){
+                    res += to_string(s.top());
+                    s.pop();
+                }
+            }
+        } 
+        return res;
+    }
+};
